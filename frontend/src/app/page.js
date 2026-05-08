795ecb6116caeb53f9bc978000956ccd8ee3a7de
Srija <srijareddy327m@gmail.com>
2026-05-08 21:39:03 +0530

    hi changes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,10 @@
-
-export default function a(){
-  return <h1>hi</h1>
+export default function A() {
+  return (
+    <>
+      <h1>hi</h1>
+      <div>
+        <h1>ji</h1>
+      </div>
+    </>
+  )
 }
